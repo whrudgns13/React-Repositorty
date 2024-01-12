3f9793fa8c00d82118661d88477004b7a6da511d
jokyounghun <whrudgns13@naver.com>
2024-01-12 16:13:44 +0900

    update custom hook 으로 변경

--- DATA-FETCHING/src/components/AvailablePlaces.jsx
+++ DATA-FETCHING/src/components/AvailablePlaces.jsx
@@ -3,37 +3,31 @@ import Places from './Places.jsx';
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
 import { fetchPlaces } from "../http.js";
+import { useFetch } from "../../hooks/useFetch.js";
+
+const fetchSortPlaces = async () =>{
+  const places = await fetchPlaces();
+  
+  return new Promise((resolve, reject)=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  })
+} 
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
 
-  useEffect(()=>{
-    (async () =>{
-      setIsLoading(true);
-
-      try{
-       
-        const places = await fetchPlaces();
-        
-        navigator.geolocation.getCurrentPosition((position)=>{
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsLoading(false);
-        });
-        
-      }catch(error){
-        setError(error);
-      }
-    })();
-
-  },[]);
-
+  const {
+    isLoading,
+    error,
+    fetchData : availablePlaces
+  } = useFetch(fetchSortPlaces);
+  
   if(error){
     return <Error title="An" message={error.message || 'Error'}/>
   }
